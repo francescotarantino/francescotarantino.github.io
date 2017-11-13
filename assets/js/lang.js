d0e105f9e2bc7cf46743ddd3ad249034c7f22c8e
Francesco Tarantino <2002nasce@gmail.com>
2017-11-13 22:42:53 +0100

    Added language selector.

--- assets/js/lang.js
+++ assets/js/lang.js
@@ -1,25 +1,40 @@
 //Thanks to: https://github.com/dakk
-
-var langs = ['en', 'it'];
-var langCode = '';
-var langJS = null;
 var jsonlang = null;
 
+function apply_lang(langCode) {
+	var langs = ['en', 'it'];
+	if (langCode === undefined)
+		var langCode = 'en';
+	if (langs.indexOf(langCode) == -1)
+		langCode = "en";
+	var langJS = null;
 
-var translate = function (jsdata){
-	jsonlang = jsdata;
-	$("[tkey]").each (function (index)
-	{
-		var strTr = jsdata [$(this).attr ('tkey')];
-	    $(this).html (strTr);
-	});
-	$(document).trigger("lang_loaded");
+	var translate = function (jsdata){
+		jsonlang = jsdata;
+		$("[tkey]").each (function (index)
+		{
+			var strTr = jsdata [$(this).attr ('tkey')];
+		    $(this).html (strTr);
+		});
+		$(document).trigger("lang_loaded");
+	}
+
+
+	$.getJSON('lang/'+langCode+'.json', translate);
+
+	$("#nav-lang-" + langCode).addClass("active");
 }
 
+$("#nav-lang-it").click(function () {
+	Cookies.set("lang", "it");
+	apply_lang("it");
+	$("#nav-lang-it").addClass("active");
+	$("#nav-lang-en").attr("class", "");
+});
 
-langCode = navigator.language.substr (0, 2);
-
-if (langs.indexOf(langCode) == -1)
-	langCode = "en";
-
-$.getJSON('lang/'+langCode+'.json', translate);
+$("#nav-lang-en").click(function () {
+	Cookies.set("lang", "en");
+	apply_lang("en");
+	$("#nav-lang-en").addClass("active");
+	$("#nav-lang-it").attr("class", "");
+});
