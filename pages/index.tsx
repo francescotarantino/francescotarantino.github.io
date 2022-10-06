6b3cec2602e756b61051924ccee66e0189899444
Francesco Tarantino <5649257+francescotarantino@users.noreply.github.com>
2022-10-06 22:49:12 +0200

    Name and link position on sm

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,38 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 import Scrolldown from "../components/scrolldown";
 
+const LinkIcons = () => (
+  <p className="font-sans font-bold p-4 text-2xl text-secondary-800">
+    <a href="https://t.me/francesco_tar" target="_blank" rel="noreferrer">
+      <FontAwesomeIcon icon={faTelegram} />
+    </a>
+    {"  "}
+    <a
+      href="https://github.com/francescotarantino"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <FontAwesomeIcon icon={faGithub} />
+    </a>
+    {"  "}
+    <a
+      href="https://www.linkedin.com/in/francesco-tarantino2"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <FontAwesomeIcon icon={faLinkedin} />
+    </a>
+    {"  "}
+    <a
+      href="https://twitter.com/_francescotar"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <FontAwesomeIcon icon={faTwitter} />
+    </a>
+  </p>
+);
+
 export default function Home() {
   const scrolldown = useRef(null);
 
@@ -36,55 +68,32 @@ export default function Home() {
       <div className="z-20 fixed pointer-events-none touch-none top-0 left-0 w-full h-screen">
         <div className="relative border-[1rem] md:border-[1.5rem] border-transparent w-full h-full border-primary-200">
           <div className="w-full h-full border-2 rounded-xl border-secondary-800">
-            <div className="z-30 fixed mx-20 rounded-b-xl border-t-0 border-2 border-secondary-800 backdrop-blur-sm">
+            <div className="z-30 w-auto mx-20 fixed rounded-b-xl border-t-0 border-2 border-secondary-800 backdrop-blur-sm invisible sm:visible">
               <p className="font-sans font-bold p-4 text-2xl text-secondary-800">
                 Francesco Tarantino
               </p>
             </div>
           </div>
 
-          <div className="z-30 pointer-events-auto touch-auto absolute bottom-0.5 right-0 mx-20 rounded-t-xl border-b-0 border-2 border-secondary-800 backdrop-blur-sm">
-            <p className="font-sans font-bold p-4 text-2xl text-secondary-800">
-              <a
-                href="https://t.me/francesco_tar"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <FontAwesomeIcon icon={faTelegram} />
-              </a>
-              {"  "}
-              <a
-                href="https://github.com/francescotarantino"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <FontAwesomeIcon icon={faGithub} />
-              </a>
-              {"  "}
-              <a
-                href="https://www.linkedin.com/in/francesco-tarantino2"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <FontAwesomeIcon icon={faLinkedin} />
-              </a>
-              {"  "}
-              <a
-                href="https://twitter.com/_francescotar"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <FontAwesomeIcon icon={faTwitter} />
-              </a>
-            </p>
+          <div className="z-30 pointer-events-auto touch-auto absolute bottom-0.5 right-0 mx-20 rounded-t-xl border-b-0 border-2 border-secondary-800 backdrop-blur-sm invisible sm:visible">
+            <LinkIcons />
           </div>
         </div>
       </div>
 
-      <div className="w-screen h-screen bg-primary-200" data-scrollbar>
-        <div className="flex h-screen w-screen px-20 flex-col md:flex-row">
-          <div className="m-auto grow justify-center flex flex-col pointer-events-none">
-            <p className="font-display font-extrabold text-4xl sm:text-6xl lg:text-7xl uppercase text-secondary-800">
+      <div
+        className="w-screen h-screen bg-primary-200"
+        style={{ height: "-webkit-fill-available" }}
+        data-scrollbar
+      >
+        <div className="flex sm:h-screen w-screen px-20 flex-col md:flex-row">
+          <div className="font-sans font-bold pt-8 text-2xl text-secondary-800 text-center sm:hidden">
+            Francesco Tarantino
+            <LinkIcons />
+          </div>
+
+          <div className="m-auto grow justify-center flex flex-col pointer-events-none text-center md:text-left">
+            <p className="font-display font-extrabold text-6xl lg:text-7xl uppercase text-secondary-800">
               Computer engineer
             </p>
             <p className="font-display text-xl sm:text-4xl text-secondary-800/75">
@@ -108,13 +117,14 @@ export default function Home() {
         </div>
 
         <div className="flex h-screen px-20 flex-col-reverse md:flex-row">
-          <div className="m-auto flex-1">
+          <div className="m-auto flex-1 ">
             <p className="font-display font-extrabold text-4xl sm:text-7xl uppercase text-secondary-800">
               My projects
             </p>
+            <p className="font-display text-xl sm:text-4xl text-secondary-800/75">
+              Work in progress...
+            </p>
           </div>
-
-          <div className="flex-1"></div>
         </div>
       </div>
 
